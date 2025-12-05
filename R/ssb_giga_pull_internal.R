e06ceeb5b5c6bb185a31e58bbd33cb10e4b18171
gauskr <gaute_skrove@hotmail.com>
2025-12-05 09:46:46 +0100

    Added licence reminder to R-scripts.

--- R/ssb_giga_pull_internal.R
+++ R/ssb_giga_pull_internal.R
@@ -1,3 +1,15 @@
+# Copyright (C) 2025 Gaute Skrove
+# This file is part of the ssbtoolsR package.
+# ssbtoolsR is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# ssbtoolsR is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+
 #' @importFrom dplyr mutate summarise group_by full_join select
 #' @importFrom purrr map_int
 #' @importFrom stringr str_c str_remove
--- R/ssb_import_n_save_list_of_dfs_as_csv.R
+++ R/ssb_import_n_save_list_of_dfs_as_csv.R
@@ -1,3 +1,14 @@
+# Copyright (C) 2025 Gaute Skrove
+# This file is part of the ssbtoolsR package.
+# ssbtoolsR is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# ssbtoolsR is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
 #' SSB import and create file by tabnumber and filetype
 #'
 #' @param tabnum_list A vector of type character, with numbers corresponding to SSB/statbank-tables. written as character string, referring to a SSB-table in SSBs statbank.
